919558bc929f681fa23c2f675ff255e68c788a91
yibo.liu <1501986252@qq.com>
2019-06-28 21:42:34 +0800

    参数校验完善

--- src/main/java/com/ryml/MyApplication.java
+++ src/main/java/com/ryml/MyApplication.java
@@ -15,8 +15,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @MapperScan("/com/ryml/mapper")
 @EntityScan("/com/ryml/entity")
-public class TestApplication {
+public class MyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class,args);
+        SpringApplication.run(MyApplication.class,args);
     }
 }
--- src/main/java/com/ryml/util/ValidateResult.java
+++ src/main/java/com/ryml/util/ValidateResult.java
@@ -1,7 +1,5 @@
 package com.ryml.util;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
 

65bc7abe4061a9228790fd1ac48a5b5e888b399e
yibo.liu <1501986252@qq.com>
2019-07-01 21:06:31 +0800

    zookeeper处理

--- src/test/java/com/ryml/MyApplicationTest.java
+++ src/test/java/com/ryml/MyApplicationTest.java
@@ -1,8 +1,17 @@
 package com.ryml;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.zookeeper.CreateMode;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * description:
@@ -14,6 +23,83 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyApplicationTest {
+    private static final Logger logger = LoggerFactory.getLogger(MyApplicationTest.class);
+
+    private final static String ROOT_PATH_LOCK = "rootlock";
+
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    @Autowired
+    private CuratorFramework curatorFramework;
+
+    /**
+     * 获取分布式锁
+     */
+    public void acquireDistributedLock(String path) {
+        String keyPath = "/" + ROOT_PATH_LOCK + "/" + path;
+        while (true) {
+            try {
+                curatorFramework
+                        .create()
+                        .creatingParentsIfNeeded()
+                        .withMode(CreateMode.EPHEMERAL)
+                        .forPath(keyPath);
+                logger.info("success to acquire lock for path:{}", keyPath);
+                break;
+            } catch (Exception e) {
+                logger.info("failed to acquire lock for path:{}", keyPath);
+                logger.info("while try again .......");
+                try {
+                    if (countDownLatch.getCount() <= 0) {
+                        countDownLatch = new CountDownLatch(1);
+                    }
+                    countDownLatch.await();
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 释放分布式锁
+     */
+    public boolean releaseDistributedLock(String path) {
+        try {
+            String keyPath = "/" + ROOT_PATH_LOCK + "/" + path;
+            if (curatorFramework.checkExists().forPath(keyPath) != null) {
+                curatorFramework.delete().forPath(keyPath);
+            }
+        } catch (Exception e) {
+            logger.error("failed to release lock");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 创建 watcher 事件
+     */
+    private void addWatcher(String path) throws Exception {
+        String keyPath;
+        if (path.equals(ROOT_PATH_LOCK)) {
+            keyPath = "/" + path;
+        } else {
+            keyPath = "/" + ROOT_PATH_LOCK + "/" + path;
+        }
+        final PathChildrenCache cache = new PathChildrenCache(curatorFramework, keyPath, false);
+        cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        cache.getListenable().addListener((client, event) -> {
+            if (event.getType().equals(PathChildrenCacheEvent.Type.CHILD_REMOVED)) {
+                String oldPath = event.getData().getPath();
+                logger.info("success to release lock for path:{}", oldPath);
+                if (oldPath.contains(path)) {
+                    //释放计数器，让当前的请求获取锁
+                    countDownLatch.countDown();
+                }
+            }
+        });
+    }
 
 }
 

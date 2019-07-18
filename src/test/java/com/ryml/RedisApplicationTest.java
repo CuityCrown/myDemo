7d51182ec58dc292b6d6b20cb9e253b7ab900a15
刘一博 <yibo.liu@ucarinc.com>
2019-07-18 14:12:36 +0800

    redis客户端实现

--- src/test/java/com/ryml/RedisApplicationTest.java
+++ src/test/java/com/ryml/RedisApplicationTest.java
@@ -8,7 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -68,4 +74,33 @@ public class RedisApplicationTest {
         System.out.println("主线程结束");
     }
 
+}
+class woshi{
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(6379);
+        Socket socket = server.accept();
+        byte[] chars = new byte[64];
+        socket.getInputStream().read(chars);
+        System.out.println(new String(chars));
+    }
+}
+class nicai{
+    public static void main(String[] args) throws IOException {
+        Jedis jedis = new Jedis();
+        jedis.set("","");
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("localhost",6379),1000);
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("*3".getBytes());
+        //命令
+        outputStream.write("$3".getBytes());
+        outputStream.write("SET".getBytes());
+        //key
+        outputStream.write("$1".getBytes());
+        outputStream.write("1".getBytes());
+        //value
+        outputStream.write("$6".getBytes());
+        outputStream.write("测试".getBytes());
+        outputStream.flush();
+    }
 }
--- src/test/java/com/ryml/ZkApplicationTest.java
+++ src/test/java/com/ryml/ZkApplicationTest.java
@@ -5,9 +5,17 @@ import org.apache.zookeeper.CreateMode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
 /**
@@ -43,5 +51,11 @@ public class ZkApplicationTest {
         System.out.println(childes);
     }
 
+    @Test
+    public void Test2(){
+
+    }
+
 
 }
+

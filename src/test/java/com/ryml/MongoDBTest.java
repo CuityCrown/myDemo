96470bf87ed271d805e457f3a1004215b5ab8142
刘一博 <yibo.liu@ucarinc.com>
2019-07-30 14:36:02 +0800

    mongoDB 引入

--- src/test/java/com/ryml/MongoDBTest.java
+++ src/test/java/com/ryml/MongoDBTest.java
@@ -9,11 +9,11 @@ import org.bson.Document;
 import org.junit.Test;
 
 /**
- * description:zookeeper测试类
+ * description:MongoDB测试类
  *
  * @author 刘一博
  * @version V1.0
- * @date 2019/7/1
+ * @date 2019/7/30
  */
 public class MongoDBTest {
 

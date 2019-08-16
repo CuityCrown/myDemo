abfa87ecacf52f70ef78883c745a53128bdaf1b8
刘一博 <yibo.liu@ucarinc.com>
2019-08-16 17:45:36 +0800

    add

--- src/main/java/com/ryml/demo/io/nio/NioClient.java
+++ src/main/java/com/ryml/demo/io/nio/NioClient.java
@@ -1,5 +1,12 @@
 package com.ryml.demo.io.nio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 /**
  * description:NIO客户端
  *
@@ -9,8 +16,24 @@ package com.ryml.demo.io.nio;
  */
 public class NioClient {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost",8091));
+        socketChannel.configureBlocking(false);
+        Selector selector = Selector.open();
+        socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        while(selector.select() > 0){
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while(iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                //防止重复轮询
+                iterator.remove();
+                if (selectionKey.isReadable()){
+
+                }
+            }
+        }
     }
+
 
 }

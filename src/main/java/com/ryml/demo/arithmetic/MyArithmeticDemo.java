2c691ba68095b0bfd5537a1522232a4914443f2e
yibo.liu <1501986252@qq.com>
2019-08-27 21:18:25 +0800

    add Arithmetic

--- src/main/java/com/ryml/demo/arithmetic/MyArithmeticDemo.java
+++ src/main/java/com/ryml/demo/arithmetic/MyArithmeticDemo.java
@@ -2,6 +2,8 @@ package com.ryml.demo.arithmetic;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * description:
  *
@@ -62,7 +64,7 @@ public class MyArithmeticDemo {
 
     @Test
     public void testK(){
-        int[] s = new int[]{5,6,7,8,9,5,4,3,2,1};
+        int[] s = new int[]{9,3,2,1,6};
         executeK(s,0,s.length-1);
         for (int i : s) {
             System.out.println(i);
@@ -90,8 +92,76 @@ public class MyArithmeticDemo {
             }
         }
         s[left1] = d;
+        for (int i : s) {
+            System.out.println(i);
+        }
+        System.out.println("===============");
         executeK(s,left,left1-1);
         executeK(s,left1+1,right);
     }
+
+    @Test
+    public void test4(){
+        Node node = new Node(1,null);
+        Node node1 = new Node(2, null);
+        node.setNext(node1);
+        Node node2 = new Node(3, null);
+        node1.setNext(node2);
+        Node node3 = new Node(4, null);
+        node2.setNext(node3);
+        Node node4 = new Node(5, null);
+        node3.setNext(node4);
+        Node node5 = new Node( 6, null);
+        node4.setNext(node5);
+
+        Node node6 = testF(node);
+        System.out.println("---------------------");
+        Node node7 = node6;
+        while(node7 != null){
+            System.out.println(node7.getItem());
+            node7 = node7.getNext();
+        }
+    }
+
+    public Node testF(Node node){
+        Node node1 = node;
+        Node node2 = node.getNext();
+        Node node3;
+        while(node2 != null){
+          node3 = node2.next;
+          node2.next = node1;
+          node1 = node2;
+          node2 = node3;
+        }
+        node.next = null;
+        return node1;
+    }
+
+
+    public static class Node<E> {
+        E item;
+        Node<E> next;
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+        public E getItem() {
+            return item;
+        }
+
+        public void setItem(E item) {
+            this.item = item;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+    }
+
 
 }

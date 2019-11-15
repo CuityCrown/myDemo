d75eefab93653ba7460e0988231e050654a0896e
yibo.liu <1501986252@qq.com>
2019-11-15 11:02:12 +0800

    upadte redisConfig

--- src/main/java/com/ryml/config/redis/RedisConfiguration.java
+++ src/main/java/com/ryml/config/redis/RedisConfiguration.java
@@ -9,6 +9,28 @@ import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisPool;
 
 */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * description:
  *
@@ -32,4 +54,111 @@ public class RedisConfiguration {
     }
 
 }
+package com.planning.webconsole.controltower.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.redis.sentinel.master}")
+    private String master;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("#{'${spring.redis.sentinel.nodes}'.split(',')}")
+    private List<String> nodes;
+
+    @Autowired
+    private JedisConnectionFactory jedisConnectionFactory;
+
+    @Bean
+    @ConfigurationProperties("spring.redis")
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    redis哨兵配置
+    @Bean
+    public RedisSentinelConfiguration redisSentinelConfiguration() {
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration();
+        redisSentinelConfiguration.setPassword(password);
+        Set<RedisNode> redisNodeSet = nodes.stream().map(x -> new RedisNode(x.split(":")[0], Integer.parseInt(x.split(":")[1])))
+                .collect(Collectors.toSet());
+        redisSentinelConfiguration.setSentinels(redisNodeSet);
+        redisSentinelConfiguration.setMaster(master);
+        return redisSentinelConfiguration;
+    }
+
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(@Autowired RedisSentinelConfiguration redisSentinelConfiguration, @Autowired JedisPoolConfig jedisPoolConfig) {
+        return new JedisConnectionFactory(redisSentinelConfiguration, jedisPoolConfig);
+    }
+
+
+    配置redisTemplate并设置序列化方式
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate() {
+        //StringRedisTemplate的构造方法中默认设置了stringSerializer
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        template.setConnectionFactory(jedisConnectionFactory);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        // 初始化缓存管理器，在这里我们可以缓存的整体过期时间什么的，我这里默认没有配置
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(jedisConnectionFactory);
+        return builder.build();
+    }
+
+    自动生成key定义
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (o, method, objects) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(o.getClass().getName()).append(".");
+            sb.append(method.getName()).append(".");
+            for (Object obj : objects) {
+                sb.append(obj.toString());
+            }
+            System.out.println("keyGenerator=" + sb.toString());
+            return sb.toString();
+        };
+    }
+
+}
 */
+

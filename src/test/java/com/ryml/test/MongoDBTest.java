9345ab74f22193ca61a2cd2658bc196b28cca860
yibo.liu <1501986252@qq.com>
2020-01-22 11:48:41 +0800

    mongo处理

--- src/test/java/com/ryml/test/MongoDBTest.java
+++ src/test/java/com/ryml/test/MongoDBTest.java
@@ -1,4 +1,12 @@
-package com.ryml.test;
+package com.ryml;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.junit.Test;
 
 /**
  * description:MongoDB测试类
@@ -9,7 +17,78 @@ package com.ryml.test;
  */
 public class MongoDBTest {
 
+    MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
 
+    @Test
+    public void createCollection(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+        //创建collection
+        mongoDatabase.createCollection("student");
+
+    }
+
+    @Test
+    public void insert(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+
+        MongoCollection<Document> student = mongoDatabase.getCollection("student");
+        //插入数据
+        Document document = new Document();
+        document.append("id","2");
+        document.append("name","yyx");
+        document.append("age","25");
+        document.append("sex","女");
+        student.insertOne(document);
+    }
+
+    @Test
+    public void delete(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+
+        MongoCollection<Document> student = mongoDatabase.getCollection("student");
+
+        student.deleteOne(Filters.eq("name","yyx"));
+    }
+
+    @Test
+    public void update(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+
+        MongoCollection<Document> student = mongoDatabase.getCollection("student");
+
+        student.updateOne(Filters.eq("name", "刘一博"), new Document("$set",new Document("name","GuityCrown")));
+    }
+
+    @Test
+    public void select(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+
+        //获取collection
+        MongoCollection<Document> student = mongoDatabase.getCollection("student");
+
+        //collection数据查询
+        FindIterable<Document> documents = student.find();
+
+        for (Document document : documents) {
+            System.out.println(document.toJson());
+        }
+    }
+
+    @Test
+    public void test1(){
+        // 连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+
+        //获取collection
+        MongoCollection<Document> student = mongoDatabase.getCollection("student");
+
+
+    }
 
 }
 
